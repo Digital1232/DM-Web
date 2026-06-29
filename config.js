f52ded35cff422601543533b00c37eb1ce768564
Digital1232 <digitalmarketing@vilpower.com>
2026-06-29 18:20:32 +0530

    Fix Karthika localStatus metrics and leaderboard checks, integrate Meta Ads token and dashboard

--- config.js
+++ config.js
@@ -34,6 +34,10 @@ export const JIRA = {
 
 export const CLIENTS = ['NTT', 'Einstein', 'IVN', 'DreamDaa', 'Aladi Ezhilvanan', 'Vilpower', 'Others', 'Vilpower DM', 'Quade', 'Discussion', 'Learning', 'Nivya', 'Mr.Millet', 'Mopower', 'Iniya', '3Jo Toys', 'SalesNaany', 'University', 'Client', 'SKM', 'AshmithaSree'];
 
+export const META_ADS = {
+    accessToken: "EAAZBjdDlKPisBR5B2oxcEr7LKIh2hiZAkSyA990xiJ3lTfO93ZAL69WeXWz7V889cFADqB42DciNcOy9UZBIHXENjXVBk5lBbYcXso6MRQF0XZBVKFVu5zAKSxrYXqwcIFTPWnfPJZAeISI4T1LrsckxO0NysnZBzrlaURPMK0BK5jHAkfz8IJSxycUHG2iIdHzJP8ooZADR"
+};
+
 // Leave Approval Chains - Different employees have different approval hierarchies
 export const LEAVE_APPROVAL_CHAINS = {
     'immanuelvilpower@gmail.com': [
